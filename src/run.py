18f9a5a81b6508a7bce730d26169619799f3e58f
Maksim Vasyuk <max234858100@gmail.com>
2022-03-27 02:22:18 +0500

    dropping last batch has been added

--- src/run.py
+++ src/run.py
@@ -83,7 +83,7 @@ if __name__ == "__main__":
     test_dataset = WhaleAndDolphinDataset(dataset_path=opt.test_dataset_path, df_path=opt.test_df_path,
                                           image_size=128, transform=get_test_transform(), is_train=False)
     test_dataloader = DataLoader(
-        test_dataset, batch_size=opt.batch_size, num_workers=opt.num_workers)
+        test_dataset, batch_size=opt.batch_size, last_batch='drop', num_workers=opt.num_workers)
     test_df = test_dataset.df.copy()
 
     # define model, optimizer, criterion

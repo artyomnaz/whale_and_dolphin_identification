30f8880259795e405e707253c910d456cbf454d4
Maksim Vasyuk <max234858100@gmail.com>
2022-03-27 19:20:45 +0500

    albumentations transforms have been added

--- src/dataset.py
+++ src/dataset.py
@@ -1,3 +1,5 @@
+import albumentations as A
+from albumentations.pytorch import ToTensorV2
 import os
 
 import pandas as pd
@@ -81,10 +83,30 @@ def get_train_transform():
     Returns:
         _type_: torchvision.transforms
     """
-    transform = transforms.Compose([transforms.RandomHorizontalFlip(),
-                                    transforms.ToTensor(),
-                                    transforms.Normalize(mean=[0.485, 0.456, 0.406],
-                                                         std=[0.229, 0.224, 0.225])])
+    transform = A.Compose([
+        A.HorizontalFlip(),
+        A.ShiftScaleRotate(shift_limit=0.1, 
+                           scale_limit=0.15, 
+                           rotate_limit=60, 
+                           p=0.5),
+        A.HueSaturationValue(
+                hue_shift_limit=0.2, 
+                sat_shift_limit=0.2, 
+                val_shift_limit=0.2, 
+                p=0.5
+            ),
+        A.RandomBrightnessContrast(
+                brightness_limit=(-0.1,0.1), 
+                contrast_limit=(-0.1, 0.1), 
+                p=0.5
+            ),
+        A.Normalize(
+                mean=[0.485, 0.456, 0.406], 
+                std=[0.229, 0.224, 0.225], 
+                max_pixel_value=255.0, 
+                p=1.0
+            ),
+        ToTensorV2()], p=1.)
     return transform
 
 

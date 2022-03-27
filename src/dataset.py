60ea18deea8d15d20cdde652e2c20e6a36c963a7
Maksim Vasyuk <max234858100@gmail.com>
2022-03-28 02:06:23 +0500

    test transforms have been edited

--- src/dataset.py
+++ src/dataset.py
@@ -116,7 +116,8 @@ def get_test_transform():
     Returns:
         _type_: torchvision.transforms
     """
-    transform = transforms.Compose([transforms.ToTensor(),
-                                    transforms.Normalize(mean=[0.485, 0.456, 0.406],
-                                                         std=[0.229, 0.224, 0.225])])
+    transform = A.Compose([A.Normalize(mean=[0.485, 0.456, 0.406], 
+                                       std=[0.229, 0.224, 0.225], 
+                                       max_pixel_value=255.0, p=1.0),
+                           ToTensorV2()], p=1.)
     return transform

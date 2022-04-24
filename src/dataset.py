ed8958ec536316bc0fc12d89ebc45d33152ca4a6
artyomnaz <1998artyomnaz@mail.ru>
2022-04-24 18:01:24 +0300

    dataset

--- src/dataset.py
+++ src/dataset.py
@@ -1,9 +1,11 @@
-from typing import Callable, Dict, Optional
+from typing import Callable, Dict, Optional, Tuple
 
 import pandas as pd
 import torch
 from PIL import Image
-from torch.utils.data import Dataset
+from torch.utils.data import DataLoader, Dataset
+
+from src.modules import LitDataModule, LitModule
 
 
 class HappyWhaleDataset(Dataset):
@@ -32,3 +34,38 @@ class HappyWhaleDataset(Dataset):
 
     def __len__(self) -> int:
         return len(self.df)
+
+
+def load_eval_module(checkpoint_path: str, device: torch.device) -> LitModule:
+    module = LitModule.load_from_checkpoint(checkpoint_path)
+    module.to(device)
+    module.eval()
+
+    return module
+
+
+def load_dataloaders(
+    train_csv_encoded_folded: str,
+    test_csv: str,
+    val_fold: float,
+    image_size: int,
+    batch_size: int,
+    num_workers: int,
+) -> Tuple[DataLoader, DataLoader, DataLoader]:
+
+    datamodule = LitDataModule(
+        train_csv_encoded_folded=train_csv_encoded_folded,
+        test_csv=test_csv,
+        val_fold=val_fold,
+        image_size=image_size,
+        batch_size=batch_size,
+        num_workers=num_workers,
+    )
+
+    datamodule.setup()
+
+    train_dl = datamodule.train_dataloader()
+    val_dl = datamodule.val_dataloader()
+    test_dl = datamodule.test_dataloader()
+
+    return train_dl, val_dl, test_dl

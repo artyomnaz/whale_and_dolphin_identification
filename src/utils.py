e31c95ee78944c06845133bfb186765be0a86f9d
artyomnaz <1998artyomnaz@mail.ru>
2022-04-24 18:06:39 +0300

    utils

--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,14 @@
 from pathlib import Path
+from sklearn.preprocessing import LabelEncoder
+import numpy as np
 
 
 def get_image_path(id: str, dir: Path) -> str:
     return f"{dir / id}"
+
+
+def load_encoder(encoder_classes_path) -> LabelEncoder:
+    encoder = LabelEncoder()
+    encoder.classes_ = np.load(encoder_classes_path, allow_pickle=True)
+
+    return encoder
